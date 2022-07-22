aa62813f26f4b93d302d834e6253c4049e5beb50
Nguyen Duc Dung <dungndgcd201420@fpt.edu.vn>
2022-07-22 10:21:12 +0700

    Insert Data in Table, Adjust Controller and Modify View

--- WebApplication2/Controllers/TodoesController.cs
+++ WebApplication2/Controllers/TodoesController.cs
@@ -1,12 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.Linq;
+using WebApplication2.Data;
 
 namespace WebApplication2.Controllers
 {
+ 
+
   public class TodoesController : Controller
   {
+    private ApplicationDbContext _context;
+    public TodoesController(ApplicationDbContext context)
+    {
+      _context = context;
+    }
     public IActionResult Index()
     {
-      return View();
+      IEnumerable todoes = _context.Todoes.ToList();
+      return View(todoes);
     }
   }
 }
--- WebApplication2/Models/Todo.cs
+++ WebApplication2/Models/Todo.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using WebApplication2.Enums;
 
 namespace WebApplication2.Models
 {
   public class Todo
   {
+    [Key]
     public int Id { get; set; }
+    [Required]
+    [StringLength(255)]
     public string Description   { get; set; }
 
     public DateTime CreatedAt { get; set; } = DateTime.Now;
