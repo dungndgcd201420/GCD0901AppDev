6f1a660c8a310d67747a32aed2596a209ad413d1
Nguyen Duc Dung <dungndgcd201420@fpt.edu.vn>
2022-07-22 10:42:50 +0700

    Create a new Todo, Add Edit button

--- WebApplication2/Controllers/TodoesController.cs
+++ WebApplication2/Controllers/TodoesController.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Linq;
 using WebApplication2.Data;
+using WebApplication2.Models;
 
 namespace WebApplication2.Controllers
 {
@@ -14,10 +15,29 @@ namespace WebApplication2.Controllers
     {
       _context = context;
     }
+    [HttpGet]
     public IActionResult Index()
     {
       IEnumerable todoes = _context.Todoes.ToList();
       return View(todoes);
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+      return View();
+    }
+    public IActionResult Create(Todo todo)
+    {
+      var newTodo = new Todo
+      {
+        Description = todo.Description
+      };
+
+      _context.Add(newTodo);
+      _context.SaveChanges();
+      return RedirectToAction("Index");
+      
     }
   }
 }
